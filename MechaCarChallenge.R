6a38e5f7a6b1d8091467f8aa42a83702fe70c9b9
lesliefinlayson <84471904+lesliefinlayson@users.noreply.github.com>
2021-10-03 08:07:37 -0700

    final

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,4 +11,14 @@ head(suspensioncoil_table)
 
 total_summary <- suspensioncoil_table %>% summarize(Mean = mean(PSI),Median=median(PSI),SD=sd(PSI), Variance=var(PSI), .groups = 'keep') #create summary table 
 
-lot_summary <- suspensioncoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median=median(PSI),SD=sd(PSI), Variance=var(PSI), .groups = 'keep') #create summary table 
+
+t.test((suspensioncoil_table$PSI),mu=1500) #compare sample versus population means
+
+Lot1 <- subset(suspensioncoil_table, Manufacturing_Lot == "Lot1")
+t.test((Lot1$PSI), mu=1500)
+               
+Lot2 <- subset(suspensioncoil_table, Manufacturing_Lot == "Lot2")
+t.test((Lot2$PSI), mu=1500)
+
+Lot3 <- subset(suspensioncoil_table, Manufacturing_Lot == "Lot3")
+t.test((Lot3$PSI), mu=1500)
